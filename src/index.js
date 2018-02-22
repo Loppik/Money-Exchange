05ae89a5c7159ee104d253c5ab61811233c9e24d
Alex <shantyr_qq@mail.ru>
2018-02-22 17:29:41 +0300

    production

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let countSuchCoin = function (currency, coinValue) {
+        let count = 0;
+        if (currency >= coinValue) {
+            while (currency >= coinValue) {
+                currency -= coinValue;
+                count += 1;
+            }
+        }
+        return count;
+    };
+
+    let exchangeUseCertainCoin = function(currency, coinValue, coinSymbol, exchange) {
+        count = countSuchCoin(currency, coinValue);
+        currency -= coinValue * count;
+        if (count != 0)
+            exchange[coinSymbol] = count;
+        return currency; // remainder
+    }
+
+    
+    let exchange = {};
+    if ( currency > 0 && currency < 10000) {
+        remainder = exchangeUseCertainCoin(currency, 50, 'H', exchange);
+        remainder = exchangeUseCertainCoin(remainder, 25, 'Q', exchange);
+        remainder = exchangeUseCertainCoin(remainder, 10, 'D', exchange);
+        remainder = exchangeUseCertainCoin(remainder, 5, 'N', exchange);
+        exchangeUseCertainCoin(remainder, 1, 'P', exchange);
+    } else {
+        if (currency > 10000) {
+            exchange = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        }
+    }
+    return exchange;
 }
+
+
+
+
